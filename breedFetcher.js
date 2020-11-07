d822f567d5bb4d1fe66e9e66d7fbf8fe008bdd00
zahra-hm <zahra520@hotmail.com>
2020-11-07 00:21:52 +0000

    Fixed Linting Errors - returns description of cat breed from a data set

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,12 @@
 const request = require('request');
 let breed = process.argv.slice(2);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+  if (error) {
+    console.log('error: ', error);
+    return;
+  }
 
-
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-    if (error) {
-      console.log('error: ', error);
-      return;
-    }
-
-    const data = JSON.parse(body)[0];
-    console.log(data.description);
+  const data = JSON.parse(body)[0];
+  console.log(data.description);
 
 });
