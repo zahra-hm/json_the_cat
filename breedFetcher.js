1c5beb72dcd1721421b851fdd8514a20541fa930
zahra-hm <zahra520@hotmail.com>
2020-11-07 00:18:36 +0000

    returns description of cat breed from a data set

---
+++ breedFetcher.js
@@ -0,0 +1,14 @@
+const request = require('request');
+let breed = process.argv.slice(2);
+
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+    if (error) {
+      console.log('error: ', error);
+      return;
+    }
+
+    const data = JSON.parse(body)[0];
+    console.log(data.description);
+
+});
