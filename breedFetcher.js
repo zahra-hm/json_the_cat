10c15ca87d44d776e31745864e7dfa113eba6b1a
zahra-hm <zahra520@hotmail.com>
2020-12-04 19:47:47 +0000

    request cat breed from api, handles error and undefined breeds

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,22 @@
 const request = require('request');
 let breed = process.argv.slice(2);
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+
   if (error) {
     console.log('error: ', error);
-    return;
+    return error;
   }
 
   const data = JSON.parse(body)[0];
+
+  if (data === undefined) {
+    console.log('Breed not found!');
+    return;
+  }
+
   console.log(data.description);
 
 });
+
+
+
